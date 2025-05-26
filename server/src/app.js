039bef0e017f5013602efc4e9bb97c214c2e6fa7
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-05-26 11:53:59 +0500

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: dotenvConfig.CORS_ORIGIN,
+    origin: dotenvConfig.CORS_ORIGIN || ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
