de0f037e529ad5e6007dd69cb846a19794c48983
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-05-26 12:10:14 +0500

    cors

--- client/config/base_url.js
+++ client/config/base_url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://api.harmonix.uz";
-export const musicBaseUrl = "https://api.harmonix.uz";
+export const baseUrl = "https://api.harmonix.uz/api";
+export const musicBaseUrl = "https://api.harmonix.uz/api";
