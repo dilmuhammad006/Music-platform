fdb5b6dfd95f4b965132fb46d363edf98d707f5b
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-05-31 17:47:35 +0500

    finish

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,11 @@ import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/error.handler.middleware.js";
 import cors from "cors";
 import path from "path";
-import dotenvConfig from "./config/dotenv.config.js";
 const app = express();
 
 app.use(
   cors({
-    origin: dotenvConfig.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
