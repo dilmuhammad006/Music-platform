11db61e4fe2f6278162bf573f4a0f7f74b9cb64e
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-05-31 18:06:10 +0500

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
+app.options("/api/musics/add", cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
